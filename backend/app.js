3f59425df1ad271d589f113f2f12271859fde29b
User <user@example.com>
2026-05-23 15:01:45 +0800

    后端Express骨架搭建完成，/api/hello 跑通

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT ;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
